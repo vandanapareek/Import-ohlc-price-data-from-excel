733c99aa470fed912c30f965b2b9f88720ccbb9b
vandanapareek <vandana.pareek42@gmail.com>
2023-03-06 21:55:00 +0800

    fix search controller

--- controllers/searchController.go
+++ controllers/searchController.go
@@ -11,11 +11,11 @@ import (
 func Search(w http.ResponseWriter, r *http.Request) {
 	//set default filters
 	filterStruct := dto.SetDefault()
+	var resp Response
 
 	//validate api params
 	err := schema.NewDecoder().Decode(&filterStruct, r.URL.Query())
 	if err != nil {
-		var resp Response
 		resp.Msg = "Error in GET parameters"
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusUnprocessableEntity)
@@ -27,21 +27,24 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	details, err := csvService.SearchDetails(filterStruct)
 	if err != nil {
 		//something went wrong while searching
-		var resp Response
 		resp.Msg = err.Error()
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		json.NewEncoder(w).Encode(resp)
 		return
 	}
-	var resp Response
 	if len(details) == 0 || details == nil {
 		resp.Msg = "No record found"
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(resp)
+		return
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(details)
+		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(resp)
-	return
 
 }
 
